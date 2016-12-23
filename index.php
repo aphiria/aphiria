14e6673cb53caab18763b950cc68e3e067cf2097
opulencephp <dbyoung2@gmail.com>
2016-12-23 17:30:10 -0600

    Fixed structure of files

--- index.php
+++ index.php
@@ -1,17 +1,12 @@
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title></title>
-    </head>
-    <body>
-        <?php
-        // put your code here
-        ?>
-    </body>
-</html>
+<?php
+use Opulence\Router\Route;
+use Opulence\Router\RouteMapBuilder;
+use Opulence\Router\RouteMapBuilderRegistry;
+use Opulence\Router\Router;
+
+$routeMapBuilderRegistry = new RouteMapBuilderRegistry(new RouteParser());
+$routeMapBuilderRegistry->map(new Route("GET", "users/:userId"))
+        ->toController("UserController", "showProfile")
+        ->withName("UserProfile");
+
+$router = new Router($routeMapBuilderRegistry->buildAll());
