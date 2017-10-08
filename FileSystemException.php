5c0e610fb330c546b9357f8010e8b3f086c8d1ab
opulencephp <dbyoung2@gmail.com>
2017-10-08 16:55:55 -0500

    Deprecated the Files library for the IO library

---
+++ FileSystemException.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+ * Opulence
+ *
+ * @link      https://www.opulencephp.com
+ * @copyright Copyright (C) 2017 David Young
+ * @license   https://github.com/opulencephp/Opulence/blob/master/LICENSE.md
+ */
+
+namespace Opulence\IO;
+
+use Exception;
+
+/**
+ * Defines the exception to be thrown by the file system
+ */
+class FileSystemException extends Exception
+{
+    // Don't do anything
+}
