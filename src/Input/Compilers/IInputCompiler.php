17c8cfa5a0dcb6a3ea0b55ae1055d49c98850503
davidbyoung <dbyoung2@gmail.com>
2019-03-10 15:55:10 -0500

    Changed CS to enforce file header and strict types

--- src/Input/Compilers/IInputCompiler.php
+++ src/Input/Compilers/IInputCompiler.php
@@ -1,12 +1,14 @@
 <?php
 
-/*
- * Opulence
+/**
+ * Aphiria
  *
  * @link      https://www.aphiria.com
  * @copyright Copyright (C) 2019 David Young
  * @license   https://github.com/aphiria/console/blob/master/LICENSE.md
  */
+
+declare(strict_types=1);
 
 namespace Aphiria\Console\Input\Compilers;
 
