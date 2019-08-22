16b3f2e38133886fad068d645de775fc4ca4ff52
davidbyoung <dbyoung2@gmail.com>
2019-08-21 21:08:04 -0500

    Updated to push subtrees via splitsh-lite during the CI process

---
+++ bin/split.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/env bash
+
+GIT_USER="$1"
+GIT_ACCESS_TOKEN="$2"
+
+if [ -z "$GIT_USER" ]
+then
+    echo "No Git user specified" 1>&2
+fi
+
+if [ -z "$GIT_ACCESS_TOKEN" ]
+then
+    echo "No Git access token specified" 1>&2
+fi
+
+CURRENT_BRANCH="temp"
+REPOS=(Api Configuration Console Exceptions Middleware Net RouteAnnotations Router Serialization)
+
+function split()
+{
+    prefix=$1
+    remote=$2
+    sha=$(./bin/splitsh-lite --prefix="$prefix")
+
+    if [ -z "$sha" ]
+    then
+        echo "Empty SHA" 1>&2
+    fi
+
+    git push "$remote" "$sha:refs/heads/$CURRENT_BRANCH" -f >/dev/null 2>&1
+}
+
+git checkout $CURRENT_BRANCH
+git pull origin $CURRENT_BRANCH
+
+# Testing devops scripts
+git remote add temp https://$GIT_USER:$GIT_ACCESS_TOKEN@github.com:aphiria/temp.git >/dev/null 2>&1
+split "src/Api" "temp"
+
+#for repo in ${REPOS[@]}
+#do
+#    lower_repo=$(echo "$repo" | awk '{print tolower($0)}')
+#    git ls-remote --exit-code "$lower_repo"
+#
+#    if test $? = 1;
+#    then
+#        git remote add "$lower_repo" https://$GIT_USER:$GIT_ACCESS_TOKEN@github.com:aphiria/$lower_repo.git >/dev/null 2>&1
+#    fi
+#
+#    split "src/$repo" "$lower_repo"
+#done
