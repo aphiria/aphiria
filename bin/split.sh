4954843f72e0e669ee3492514cee2cf0e9a267ba
davidbyoung <dbyoung2@gmail.com>
2019-08-25 12:48:19 -0500

    Converted split script to use mapping of directories to repos because it was too difficult to, in some cases, lower case the directory, other times train-case them, and in others (eg IO) just lower-case them

--- bin/split.sh
+++ bin/split.sh
@@ -24,16 +24,17 @@ then
     exit 1
 fi
 
-for prefix in src/*/ ;
+dirs_to_repos=(["Api"]="api" ["Collections"]="collections" ["Configuration"]="configuration" ["DependencyInjection"]="dependency-injection" ["Exceptions"]="exceptions" ["IO"]="io" ["Middleware"]="middleware" ["Net"]="net" ["RouteAnnotations"]="route-annotations" ["Router"]="router" ["Serialization"]="serialization")
+
+for dir in ${!dirs_to_repos[@]}
 do
-    repo=$(basename "$prefix")
-    remote=$(echo "$repo" | awk '{print tolower($0)}')
+    remote=${dirs_to_repos[$dir]}
 
     echo "Adding remote $remote"
     git remote add "$remote" https://$GIT_USER:$GIT_ACCESS_TOKEN@github.com/aphiria/$remote.git >/dev/null 2>&1
 
-    echo "Splitting $repo"
-    sha=$(./bin/splitsh-lite --prefix="$prefix")
+    echo "Splitting $dir"
+    sha=$(./bin/splitsh-lite --prefix="src/$dir")
 
     if [ -z "$sha" ]
     then
@@ -42,6 +43,6 @@ do
     fi
 
     # Push to the subtree's repo, and do not leak any sensitive info in the logs
-    echo "Pushing $repo to $remote"
+    echo "Pushing $dir to $remote"
     git push "$remote" "$sha:refs/heads/$GIT_BRANCH" -f >/dev/null 2>&1
 done
