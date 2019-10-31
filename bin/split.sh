4876e085fd9ed742a2da4f656c639befdc9041a0
davidbyoung <dbyoung2@gmail.com>
2019-10-31 17:03:39 -0500

    Added support for tags in subsplits

--- bin/split.sh
+++ bin/split.sh
@@ -5,6 +5,7 @@ set -e
 GIT_BRANCH="$1"
 GIT_USER="$2"
 GIT_ACCESS_TOKEN="$3"
+GIT_TAG="$4"
 
 if [ -z "$GIT_BRANCH" ]
 then
@@ -29,20 +30,38 @@ declare -A dirs_to_repos=(["Api"]="api" ["Collections"]="collections" ["Configur
 for dir in "${!dirs_to_repos[@]}"
 do
     remote=${dirs_to_repos[$dir]}
-
-    echo "Adding remote $remote"
-    git remote add "$remote" https://$GIT_USER:$GIT_ACCESS_TOKEN@github.com/aphiria/$remote.git >/dev/null 2>&1
-
-    echo "Splitting $dir"
-    sha=$(./bin/splitsh-lite --prefix="src/$dir")
-
-    if [ -z "$sha" ]
-    then
-        echo "Empty SHA"
-        exit 1
-    fi
+    remote_uri="https://$GIT_USER:$GIT_ACCESS_TOKEN@github.com/aphiria/$remote.git"
 
     # Push to the subtree's repo, and do not leak any sensitive info in the logs
-    echo "Pushing $dir to $remote"
-    git push "$remote" "$sha:refs/heads/$GIT_BRANCH" -f >/dev/null 2>&1
+    if [ -z "$GIT_TAG" ]
+    then
+        echo "Adding remote $remote"
+        git remote add "$remote" "$remote_uri" >/dev/null 2>&1
+
+        echo "Splitting $dir"
+        sha=$(./bin/splitsh-lite --prefix="src/$dir")
+
+        if [ -z "$sha" ]
+        then
+            echo "Empty SHA"
+            exit 1
+        fi
+
+        echo "Pushing $dir to $remote"
+        git push "$remote" "$sha:refs/heads/$GIT_BRANCH" -f >/dev/null 2>&1
+    else
+        tmp_split_dir="/tmp/tag-split"
+        echo "Creating $tmp_split_dir for $remote"
+        rm -rf $tmp_split_dir
+        mkdir $tmp_split_dir
+
+        (
+            echo "Creating $GIT_TAG for $remote"
+            cd $tmp_split_dir
+            git clone "$remote_uri"
+            git checkout master
+            git tag "$GIT_TAG"
+            git push origin --tags
+        )
+    fi
 done
