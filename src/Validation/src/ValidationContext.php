09c6a3e9a9e7f14c826eebc3186aef8712c292df
davidbyoung <dbyoung2@gmail.com>
2019-12-04 08:58:15 -0600

    Added circular dependency detection logic to the ValidationContext

--- src/Validation/src/ValidationContext.php
+++ src/Validation/src/ValidationContext.php
@@ -23,15 +23,30 @@ final class ValidationContext
     private $value;
     /** @var ErrorCollection The errors that have occurred during validation */
     private ErrorCollection $errors;
+    /** @var string[] The list of object hash IDs we'll use to detect circular dependencies */
+    private array $objectHashIds = [];
 
     /**
      * @param mixed $value The value being validated
      * @param ErrorCollection|null $errors The errors that have occurred during validation
+     * @throws CircularDependencyException Thrown if a circular dependency was detected
      */
     public function __construct($value, ErrorCollection $errors = null)
     {
         $this->value = $value;
         $this->errors = $errors ?? new ErrorCollection();
+
+        // TODO: I think I need a concept of a parent context in the case of recursive validation.  If that's the case, I'll need to change the circular dependency logic to invoke a method recursively up the parent context chain.
+
+        if (\is_object($this->value)) {
+            $objectHashId = \spl_object_hash($this->value);
+
+            if (isset($this->objectHashIds[$objectHashId])) {
+                throw new CircularDependencyException('Circular dependency on ' . \get_class($value) . ' detected');
+            }
+
+            $this->objectHashIds[\spl_object_hash($this->value)] = true;
+        }
     }
 
     /**
