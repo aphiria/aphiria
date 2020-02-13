48ca173a680ca86f573c7578f00b83d13a40181c
davidbyoung <dbyoung2@gmail.com>
2020-02-12 19:13:14 -0600

    Updated copyright year

--- src/Router/src/UriTemplates/Constraints/NotInConstraint.php
+++ src/Router/src/UriTemplates/Constraints/NotInConstraint.php
@@ -4,7 +4,7 @@
  * Aphiria
  *
  * @link      https://www.aphiria.com
- * @copyright Copyright (C) 2019 David Young
+ * @copyright Copyright (C) 2020 David Young
  * @license   https://github.com/aphiria/aphiria/blob/master/LICENSE.md
  */
 
