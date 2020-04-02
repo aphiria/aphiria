19f26c9cd1e4c6749631e3ecc66e6d14652a0ce7
davidbyoung <dbyoung2@gmail.com>
2020-04-01 19:36:26 -0500

    Removed unnecessary code from validation library's PHP Unit bootstrapper

--- src/Validation/phpunit.bootstrap.php
+++ src/Validation/phpunit.bootstrap.php
@@ -9,12 +9,8 @@
  */
 
 use Doctrine\Common\Annotations\AnnotationRegistry;
-use Opis\Closure\ClosureStream;
 
 $autoloader = require __DIR__.'/vendor/autoload.php';
-
-/** @link https://github.com/opis/closure/issues/33 */
-ClosureStream::register();
 
 /** @link https://github.com/schmittjoh/serializer/issues/855 */
 AnnotationRegistry::registerLoader([$autoloader, 'loadClass']);
