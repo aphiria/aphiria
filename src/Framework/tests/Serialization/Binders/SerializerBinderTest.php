618eb2b1d491e69860e9525d40f8b6c8b1cb8d36
davidbyoung <dbyoung2@gmail.com>
2020-04-17 07:57:46 -0500

    Finished testing serializer binder

--- src/Framework/tests/Serialization/Binders/SerializerBinderTest.php
+++ src/Framework/tests/Serialization/Binders/SerializerBinderTest.php
@@ -18,6 +18,12 @@ use Aphiria\DependencyInjection\IContainer;
 use Aphiria\Framework\Serialization\Binders\SerializerBinder;
 use Aphiria\Serialization\Encoding\CamelCasePropertyNameFormatter;
 use Aphiria\Serialization\Encoding\EncoderRegistry;
+use Aphiria\Serialization\Encoding\EncodingContext;
+use Aphiria\Serialization\Encoding\IPropertyNameFormatter;
+use Aphiria\Serialization\FormUrlEncodedSerializer;
+use Aphiria\Serialization\ISerializer;
+use Aphiria\Serialization\JsonSerializer;
+use Aphiria\Serialization\TypeResolver;
 use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
 
@@ -53,7 +59,85 @@ class SerializerBinderTest extends TestCase
         $config['aphiria']['serialization']['propertyNameFormatter'] = CamelCasePropertyNameFormatter::class;
         GlobalConfiguration::addConfigurationSource(new HashTableConfiguration($config));
         $this->binder->bind($this->container);
-        // TODO: Still need to check the encoders to see if the right formatter was registered
+        $classWithSnakeCaseProperties = new class() {
+            public string $snake_case = 'foo';
+        };
+        $encodedClass = $this->encoders->getEncoderForType(TypeResolver::resolveType($classWithSnakeCaseProperties))
+            ->encode($classWithSnakeCaseProperties, new EncodingContext());
+        $this->assertEquals(['snakeCase' => 'foo'], $encodedClass);
+    }
+
+    public function testJsonSerializerIsInstantiatedDirectly(): void
+    {
+        $config = self::getBaseConfig();
+        $config['aphiria']['serialization']['serializers'][] = JsonSerializer::class;
+        GlobalConfiguration::addConfigurationSource(new HashTableConfiguration($config));
+        $this->container->expects($this->at(1))
+            ->method('bindInstance')
+            ->with(JsonSerializer::class, $this->isInstanceOf(JsonSerializer::class));
+        $this->binder->bind($this->container);
+    }
+
+    public function testFormUrlEncodedSerializerIsInstantiatedDirectly(): void
+    {
+        $config = self::getBaseConfig();
+        $config['aphiria']['serialization']['serializers'][] = FormUrlEncodedSerializer::class;
+        GlobalConfiguration::addConfigurationSource(new HashTableConfiguration($config));
+        $this->container->expects($this->at(1))
+            ->method('bindInstance')
+            ->with(FormUrlEncodedSerializer::class, $this->isInstanceOf(FormUrlEncodedSerializer::class));
+        $this->binder->bind($this->container);
+    }
+
+    public function testNoPropertyNameFormatterBeingSpecifiedDoesNotUseADefaultOne(): void
+    {
+        GlobalConfiguration::addConfigurationSource(new HashTableConfiguration(self::getBaseConfig()));
+        $this->binder->bind($this->container);
+        $classWithSnakeCaseProperties = new class() {
+            public string $snake_case = 'foo';
+        };
+        $encodedClass = $this->encoders->getEncoderForType(TypeResolver::resolveType($classWithSnakeCaseProperties))
+            ->encode($classWithSnakeCaseProperties, new EncodingContext());
+        $this->assertEquals(['snake_case' => 'foo'], $encodedClass);
+    }
+
+    public function testUnknownPropertyNameFormatterIsResolved(): void
+    {
+        $propertyNameFormatter = $this->createMock(IPropertyNameFormatter::class);
+        $propertyNameFormatter->expects($this->once())
+            ->method('formatPropertyName')
+            ->with('snake_case')
+            ->willReturn('SNAKECASE');
+        $config = self::getBaseConfig();
+        $config['aphiria']['serialization']['propertyNameFormatter'] = \get_class($propertyNameFormatter);
+        GlobalConfiguration::addConfigurationSource(new HashTableConfiguration($config));
+        $this->container->expects($this->at(1))
+            ->method('resolve')
+            ->with(\get_class($propertyNameFormatter))
+            ->willReturn($propertyNameFormatter);
+        $this->binder->bind($this->container);
+        $classWithSnakeCaseProperties = new class() {
+            public string $snake_case = 'foo';
+        };
+        $encodedClass = $this->encoders->getEncoderForType(TypeResolver::resolveType($classWithSnakeCaseProperties))
+            ->encode($classWithSnakeCaseProperties, new EncodingContext());
+        $this->assertEquals(['SNAKECASE' => 'foo'], $encodedClass);
+    }
+
+    public function testUnknownSerializerIsResolved(): void
+    {
+        $serializer = $this->createMock(ISerializer::class);
+        $config = self::getBaseConfig();
+        $config['aphiria']['serialization']['serializers'][] = \get_class($serializer);
+        GlobalConfiguration::addConfigurationSource(new HashTableConfiguration($config));
+        $this->container->expects($this->at(1))
+            ->method('resolve')
+            ->with(\get_class($serializer))
+            ->willReturn($serializer);
+        $this->container->expects($this->at(2))
+            ->method('bindInstance')
+            ->with(\get_class($serializer), $serializer);
+        $this->binder->bind($this->container);
     }
 
     /**
