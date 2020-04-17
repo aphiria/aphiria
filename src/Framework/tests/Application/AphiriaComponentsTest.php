df68327b6946a34ae2b3a809348059da9183cd03
davidbyoung <dbyoung2@gmail.com>
2020-04-17 08:34:52 -0500

    Added more tests to AphiriaComponents, added command binder tests

--- src/Framework/tests/Application/AphiriaComponentsTest.php
+++ src/Framework/tests/Application/AphiriaComponentsTest.php
@@ -19,6 +19,7 @@ use Aphiria\Console\Commands\CommandRegistry;
 use Aphiria\Console\Output\IOutput;
 use Aphiria\DependencyInjection\Binders\Binder;
 use Aphiria\DependencyInjection\Binders\IBinderDispatcher;
+use Aphiria\DependencyInjection\Container;
 use Aphiria\DependencyInjection\IContainer;
 use Aphiria\Framework\Application\AphiriaComponents;
 use Aphiria\Framework\Console\Components\CommandComponent;
@@ -29,6 +30,7 @@ use Aphiria\Framework\Routing\Components\RouterComponent;
 use Aphiria\Framework\Serialization\Components\SerializerComponent;
 use Aphiria\Framework\Validation\Components\ValidationComponent;
 use Aphiria\Middleware\MiddlewareBinding;
+use Aphiria\Middleware\MiddlewareCollection;
 use Aphiria\Net\Http\IHttpResponseMessage;
 use Aphiria\Routing\Builders\RouteBuilderRegistry;
 use Aphiria\Serialization\Encoding\IEncoder;
@@ -50,6 +52,7 @@ class AphiriaComponentsTest extends TestCase
     protected function setUp(): void
     {
         $this->appBuilder = $this->createMock(IApplicationBuilder::class);
+        Container::$globalInstance = new Container();
     }
 
     public function testWithBinderDispatcherRegisterBinderDispatcherToComponent(): void
@@ -76,6 +79,30 @@ class AphiriaComponentsTest extends TestCase
             }
         };
         $component->build($this->appBuilder, $binderDispatcher);
+    }
+
+    public function testWithBinderDispatcherRegistersComponentIfItIsNotRegisteredYet(): void
+    {
+        $this->appBuilder->expects($this->at(0))
+            ->method('hasComponent')
+            ->with(BinderComponent::class)
+            ->willReturn(false);
+        $this->appBuilder->expects($this->at(1))
+            ->method('withComponent')
+            ->with($this->isInstanceOf(BinderComponent::class), 0);
+        $this->appBuilder->expects($this->at(2))
+            ->method('getComponent')
+            ->with(BinderComponent::class)
+            ->willReturn($this->createMock(BinderComponent::class));
+        $component = new class() {
+            use AphiriaComponents;
+
+            public function build(IApplicationBuilder $appBuilder, IBinderDispatcher $binderDispatcher): void
+            {
+                $this->withBinderDispatcher($appBuilder, $binderDispatcher);
+            }
+        };
+        $component->build($this->appBuilder, $this->createMock(IBinderDispatcher::class));
     }
 
     public function testWithBindersRegistersBindersToComponent(): void
@@ -109,6 +136,30 @@ class AphiriaComponentsTest extends TestCase
         $component->build($this->appBuilder, $binder);
     }
 
+    public function testWithBindersRegistersComponentIfItIsNotRegisteredYet(): void
+    {
+        $this->appBuilder->expects($this->at(0))
+            ->method('hasComponent')
+            ->with(BinderComponent::class)
+            ->willReturn(false);
+        $this->appBuilder->expects($this->at(1))
+            ->method('withComponent')
+            ->with($this->isInstanceOf(BinderComponent::class), 0);
+        $this->appBuilder->expects($this->at(2))
+            ->method('getComponent')
+            ->with(BinderComponent::class)
+            ->willReturn($this->createMock(BinderComponent::class));
+        $component = new class() {
+            use AphiriaComponents;
+
+            public function build(IApplicationBuilder $appBuilder, Binder $binder): void
+            {
+                $this->withBinders($appBuilder, $binder);
+            }
+        };
+        $component->build($this->appBuilder, $this->createMock(Binder::class));
+    }
+
     public function testWithCommandAnnotationsConfiguresComponentToHaveAnnotations(): void
     {
         $expectedComponent = $this->createMock(CommandComponent::class);
@@ -122,6 +173,30 @@ class AphiriaComponentsTest extends TestCase
             ->method('getComponent')
             ->with(CommandComponent::class)
             ->willReturn($expectedComponent);
+        $component = new class() {
+            use AphiriaComponents;
+
+            public function build(IApplicationBuilder $appBuilder): void
+            {
+                $this->withCommandAnnotations($appBuilder);
+            }
+        };
+        $component->build($this->appBuilder);
+    }
+
+    public function testWithCommandAnnotationsRegistersComponentIfItIsNotRegisteredYet(): void
+    {
+        $this->appBuilder->expects($this->at(0))
+            ->method('hasComponent')
+            ->with(CommandComponent::class)
+            ->willReturn(false);
+        $this->appBuilder->expects($this->at(1))
+            ->method('withComponent')
+            ->with($this->isInstanceOf(CommandComponent::class));
+        $this->appBuilder->expects($this->at(2))
+            ->method('getComponent')
+            ->with(CommandComponent::class)
+            ->willReturn($this->createMock(CommandComponent::class));
         $component = new class() {
             use AphiriaComponents;
 
@@ -156,6 +231,31 @@ class AphiriaComponentsTest extends TestCase
                 $this->withCommands($appBuilder, $callback);
             }
         };
+        $component->build($this->appBuilder, $callback);
+    }
+
+    public function testWithCommandsRegistersComponentIfItIsNotRegisteredYet(): void
+    {
+        $this->appBuilder->expects($this->at(0))
+            ->method('hasComponent')
+            ->with(CommandComponent::class)
+            ->willReturn(false);
+        $this->appBuilder->expects($this->at(1))
+            ->method('withComponent')
+            ->with($this->isInstanceOf(CommandComponent::class));
+        $this->appBuilder->expects($this->at(2))
+            ->method('getComponent')
+            ->with(CommandComponent::class)
+            ->willReturn($this->createMock(CommandComponent::class));
+        $component = new class() {
+            use AphiriaComponents;
+
+            public function build(IApplicationBuilder $appBuilder, Closure $callback): void
+            {
+                $this->withCommands($appBuilder, $callback);
+            }
+        };
+        $callback = fn (CommandRegistry $commands) => null;
         $component->build($this->appBuilder, $callback);
     }
 
@@ -206,6 +306,31 @@ class AphiriaComponentsTest extends TestCase
         $component->build($this->appBuilder, Exception::class, $outputWriter);
     }
 
+    public function testWithConsoleExceptionOutputWriterRegistersComponentIfItIsNotRegisteredYet(): void
+    {
+        $this->appBuilder->expects($this->at(0))
+            ->method('hasComponent')
+            ->with(ExceptionHandlerComponent::class)
+            ->willReturn(false);
+        $this->appBuilder->expects($this->at(1))
+            ->method('withComponent')
+            ->with($this->isInstanceOf(ExceptionHandlerComponent::class));
+        $this->appBuilder->expects($this->at(2))
+            ->method('getComponent')
+            ->with(ExceptionHandlerComponent::class)
+            ->willReturn($this->createMock(ExceptionHandlerComponent::class));
+        $component = new class() {
+            use AphiriaComponents;
+
+            public function build(IApplicationBuilder $appBuilder, string $exceptionType, Closure $callback): void
+            {
+                $this->withConsoleExceptionOutputWriter($appBuilder, $exceptionType, $callback);
+            }
+        };
+        $callback = fn (Exception $ex, IOutput $output) => null;
+        $component->build($this->appBuilder, Exception::class, $callback);
+    }
+
     public function testWithEncodersConfiguresComponentToHaveEncoders(): void
     {
         $encoder = $this->createMock(IEncoder::class);
@@ -230,6 +355,30 @@ class AphiriaComponentsTest extends TestCase
             }
         };
         $component->build($this->appBuilder, 'foo', $encoder);
+    }
+
+    public function testWithEncoderRegistersComponentIfItIsNotRegisteredYet(): void
+    {
+        $this->appBuilder->expects($this->at(0))
+            ->method('hasComponent')
+            ->with(SerializerComponent::class)
+            ->willReturn(false);
+        $this->appBuilder->expects($this->at(1))
+            ->method('withComponent')
+            ->with($this->isInstanceOf(SerializerComponent::class));
+        $this->appBuilder->expects($this->at(2))
+            ->method('getComponent')
+            ->with(SerializerComponent::class)
+            ->willReturn($this->createMock(SerializerComponent::class));
+        $component = new class() {
+            use AphiriaComponents;
+
+            public function build(IApplicationBuilder $appBuilder, string $type, IEncoder $encoder): void
+            {
+                $this->withEncoder($appBuilder, $type, $encoder);
+            }
+        };
+        $component->build($this->appBuilder, self::class, $this->createMock(IEncoder::class));
     }
 
     public function testWithHttpResponseFactoryConfiguresComponentToHaveFactory(): void
@@ -261,6 +410,31 @@ class AphiriaComponentsTest extends TestCase
         $component->build($this->appBuilder, Exception::class, $responseFactory);
     }
 
+    public function testWithHttpExceptionResponseFactoryRegistersComponentIfItIsNotRegisteredYet(): void
+    {
+        $this->appBuilder->expects($this->at(0))
+            ->method('hasComponent')
+            ->with(ExceptionHandlerComponent::class)
+            ->willReturn(false);
+        $this->appBuilder->expects($this->at(1))
+            ->method('withComponent')
+            ->with($this->isInstanceOf(ExceptionHandlerComponent::class));
+        $this->appBuilder->expects($this->at(2))
+            ->method('getComponent')
+            ->with(ExceptionHandlerComponent::class)
+            ->willReturn($this->createMock(ExceptionHandlerComponent::class));
+        $component = new class() {
+            use AphiriaComponents;
+
+            public function build(IApplicationBuilder $appBuilder, string $exceptionType, Closure $factory): void
+            {
+                $this->withHttpExceptionResponseFactory($appBuilder, $exceptionType, $factory);
+            }
+        };
+        $callback = fn (Exception $ex) => $this->createMock(IHttpResponseMessage::class);
+        $component->build($this->appBuilder, Exception::class, $callback);
+    }
+
     public function testWithGlobalMiddlewareConfiguresComponentToHaveMiddleware(): void
     {
         $middlewareBinding = new MiddlewareBinding('foo');
@@ -290,6 +464,55 @@ class AphiriaComponentsTest extends TestCase
         $component->build($this->appBuilder, $middlewareBinding, 1);
     }
 
+    public function testWithGlobalMiddlewareRegistersComponentIfItIsNotRegisteredYet(): void
+    {
+        $this->appBuilder->expects($this->at(0))
+            ->method('hasComponent')
+            ->with(MiddlewareComponent::class)
+            ->willReturn(false);
+        $this->appBuilder->expects($this->at(1))
+            ->method('withComponent')
+            ->with($this->isInstanceOf(MiddlewareComponent::class));
+        $this->appBuilder->expects($this->at(2))
+            ->method('getComponent')
+            ->with(MiddlewareComponent::class)
+            ->willReturn($this->createMock(MiddlewareComponent::class));
+        $component = new class() {
+            use AphiriaComponents;
+
+            public function build(IApplicationBuilder $appBuilder, MiddlewareBinding $middlewareBinding): void
+            {
+                $this->withGlobalMiddleware($appBuilder, $middlewareBinding);
+            }
+        };
+        $component->build($this->appBuilder, new MiddlewareBinding('foo'));
+    }
+
+    public function testWithGlobalMiddlewareRegistersComponentIfItIsNotRegisteredYetAndUsesBoundMiddlewareCollection(): void
+    {
+        Container::$globalInstance->bindInstance(MiddlewareCollection::class, new MiddlewareCollection());
+        $this->appBuilder->expects($this->at(0))
+            ->method('hasComponent')
+            ->with(MiddlewareComponent::class)
+            ->willReturn(false);
+        $this->appBuilder->expects($this->at(1))
+            ->method('withComponent')
+            ->with($this->isInstanceOf(MiddlewareComponent::class));
+        $this->appBuilder->expects($this->at(2))
+            ->method('getComponent')
+            ->with(MiddlewareComponent::class)
+            ->willReturn($this->createMock(MiddlewareComponent::class));
+        $component = new class() {
+            use AphiriaComponents;
+
+            public function build(IApplicationBuilder $appBuilder, MiddlewareBinding $middlewareBinding): void
+            {
+                $this->withGlobalMiddleware($appBuilder, $middlewareBinding);
+            }
+        };
+        $component->build($this->appBuilder, new MiddlewareBinding('foo'));
+    }
+
     public function testWithLogLevelFactoryConfiguresComponentToHaveFactory(): void
     {
         $logLevelFactory = fn (Exception $ex) => LogLevel::ALERT;
@@ -316,6 +539,31 @@ class AphiriaComponentsTest extends TestCase
                 $this->withLogLevelFactory($appBuilder, $exceptionType, $logLevelFactory);
             }
         };
+        $component->build($this->appBuilder, Exception::class, $logLevelFactory);
+    }
+
+    public function testWithLogLevelFactoryRegistersComponentIfItIsNotRegisteredYet(): void
+    {
+        $this->appBuilder->expects($this->at(0))
+            ->method('hasComponent')
+            ->with(ExceptionHandlerComponent::class)
+            ->willReturn(false);
+        $this->appBuilder->expects($this->at(1))
+            ->method('withComponent')
+            ->with($this->isInstanceOf(ExceptionHandlerComponent::class));
+        $this->appBuilder->expects($this->at(2))
+            ->method('getComponent')
+            ->with(ExceptionHandlerComponent::class)
+            ->willReturn($this->createMock(ExceptionHandlerComponent::class));
+        $component = new class() {
+            use AphiriaComponents;
+
+            public function build(IApplicationBuilder $appBuilder, string $exceptionType, Closure $logLevelFactory): void
+            {
+                $this->withLogLevelFactory($appBuilder, $exceptionType, $logLevelFactory);
+            }
+        };
+        $logLevelFactory = fn (Exception $ex) => LogLevel::ALERT;
         $component->build($this->appBuilder, Exception::class, $logLevelFactory);
     }
 
@@ -382,6 +630,31 @@ class AphiriaComponentsTest extends TestCase
         $component->build($this->appBuilder, $callback);
     }
 
+    public function testWithObjectConstraintsRegistersComponentIfItIsNotRegisteredYet(): void
+    {
+        $this->appBuilder->expects($this->at(0))
+            ->method('hasComponent')
+            ->with(ValidationComponent::class)
+            ->willReturn(false);
+        $this->appBuilder->expects($this->at(1))
+            ->method('withComponent')
+            ->with($this->isInstanceOf(ValidationComponent::class));
+        $this->appBuilder->expects($this->at(2))
+            ->method('getComponent')
+            ->with(ValidationComponent::class)
+            ->willReturn($this->createMock(ValidationComponent::class));
+        $component = new class() {
+            use AphiriaComponents;
+
+            public function build(IApplicationBuilder $appBuilder, Closure $callback): void
+            {
+                $this->withObjectConstraints($appBuilder, $callback);
+            }
+        };
+        $factory = fn (ObjectConstraintsRegistry $objectConstraints) => null;
+        $component->build($this->appBuilder, $factory);
+    }
+
     public function testWithRouteAnnotationsConfiguresComponentToHaveAnnotations(): void
     {
         $expectedComponent = $this->createMock(RouterComponent::class);
@@ -395,6 +668,30 @@ class AphiriaComponentsTest extends TestCase
             ->method('getComponent')
             ->with(RouterComponent::class)
             ->willReturn($expectedComponent);
+        $component = new class() {
+            use AphiriaComponents;
+
+            public function build(IApplicationBuilder $appBuilder): void
+            {
+                $this->withRouteAnnotations($appBuilder);
+            }
+        };
+        $component->build($this->appBuilder);
+    }
+
+    public function testWithRouteAnnotationsRegistersComponentIfItIsNotRegisteredYet(): void
+    {
+        $this->appBuilder->expects($this->at(0))
+            ->method('hasComponent')
+            ->with(RouterComponent::class)
+            ->willReturn(false);
+        $this->appBuilder->expects($this->at(1))
+            ->method('withComponent')
+            ->with($this->isInstanceOf(RouterComponent::class));
+        $this->appBuilder->expects($this->at(2))
+            ->method('getComponent')
+            ->with(RouterComponent::class)
+            ->willReturn($this->createMock(RouterComponent::class));
         $component = new class() {
             use AphiriaComponents;
 
@@ -432,6 +729,31 @@ class AphiriaComponentsTest extends TestCase
         $component->build($this->appBuilder, $callback);
     }
 
+    public function testWithRoutesRegistersComponentIfItIsNotRegisteredYet(): void
+    {
+        $this->appBuilder->expects($this->at(0))
+            ->method('hasComponent')
+            ->with(RouterComponent::class)
+            ->willReturn(false);
+        $this->appBuilder->expects($this->at(1))
+            ->method('withComponent')
+            ->with($this->isInstanceOf(RouterComponent::class));
+        $this->appBuilder->expects($this->at(2))
+            ->method('getComponent')
+            ->with(RouterComponent::class)
+            ->willReturn($this->createMock(RouterComponent::class));
+        $component = new class() {
+            use AphiriaComponents;
+
+            public function build(IApplicationBuilder $appBuilder, Closure $callback): void
+            {
+                $this->withRoutes($appBuilder, $callback);
+            }
+        };
+        $callback = fn (RouteBuilderRegistry $routeBuilders) => null;
+        $component->build($this->appBuilder, $callback);
+    }
+
     public function testWithValidatorAnnotationsConfiguresComponentToHaveAnnotations(): void
     {
         $expectedComponent = $this->createMock(ValidationComponent::class);
@@ -445,6 +767,30 @@ class AphiriaComponentsTest extends TestCase
             ->method('getComponent')
             ->with(ValidationComponent::class)
             ->willReturn($expectedComponent);
+        $component = new class() {
+            use AphiriaComponents;
+
+            public function build(IApplicationBuilder $appBuilder): void
+            {
+                $this->withValidatorAnnotations($appBuilder);
+            }
+        };
+        $component->build($this->appBuilder);
+    }
+
+    public function testWithValidatorAnnotationsComponentIfItIsNotRegisteredYet(): void
+    {
+        $this->appBuilder->expects($this->at(0))
+            ->method('hasComponent')
+            ->with(ValidationComponent::class)
+            ->willReturn(false);
+        $this->appBuilder->expects($this->at(1))
+            ->method('withComponent')
+            ->with($this->isInstanceOf(ValidationComponent::class));
+        $this->appBuilder->expects($this->at(2))
+            ->method('getComponent')
+            ->with(ValidationComponent::class)
+            ->willReturn($this->createMock(ValidationComponent::class));
         $component = new class() {
             use AphiriaComponents;
 
---
+++ src/Framework/tests/Console/Binders/CommandBinderTest.php
@@ -0,0 +1,93 @@
+<?php
+
+/**
+ * Aphiria
+ *
+ * @link      https://www.aphiria.com
+ * @copyright Copyright (C) 2020 David Young
+ * @license   https://github.com/aphiria/aphiria/blob/master/LICENSE.md
+ */
+
+declare(strict_types=1);
+
+namespace Aphiria\Framework\Tests\Console\Binders;
+
+use Aphiria\Configuration\GlobalConfiguration;
+use Aphiria\Configuration\HashTableConfiguration;
+use Aphiria\Console\Commands\Annotations\AnnotationCommandRegistrant;
+use Aphiria\Console\Commands\CommandRegistrantCollection;
+use Aphiria\Console\Commands\CommandRegistry;
+use Aphiria\DependencyInjection\IContainer;
+use Aphiria\Framework\Console\Binders\CommandBinder;
+use PHPUnit\Framework\MockObject\MockObject;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Tests the command binder
+ */
+class CommandBinderTest extends TestCase
+{
+    /** @var IContainer|MockObject */
+    private IContainer $container;
+    private CommandBinder $binder;
+    private ?string $currEnvironment;
+
+    protected function setUp(): void
+    {
+        $this->binder = new CommandBinder();
+        $this->container = $this->createMock(IContainer::class);
+        GlobalConfiguration::resetConfigurationSources();
+        $this->currEnvironment = getenv('APP_ENV') ?: null;
+
+        // Some universal assertions
+        $this->container->expects($this->at(0))
+            ->method('bindInstance')
+            ->with(CommandRegistry::class, $this->isInstanceOf(CommandRegistry::class));
+        $this->container->expects($this->at(1))
+            ->method('bindInstance')
+            ->with(CommandRegistrantCollection::class, $this->isInstanceOf(CommandRegistrantCollection::class));
+    }
+
+    protected function tearDown(): void
+    {
+        // Restore the environment name
+        if ($this->currEnvironment !== null) {
+            putenv("APP_ENV={$this->currEnvironment}");
+        }
+    }
+
+    public function testAnnotationRegistrantIsRegistered(): void
+    {
+        GlobalConfiguration::addConfigurationSource(new HashTableConfiguration(self::getBaseConfig()));
+        $this->container->expects($this->at(2))
+            ->method('bindInstance')
+            ->with(AnnotationCommandRegistrant::class, $this->isInstanceOf(AnnotationCommandRegistrant::class));
+        $this->binder->bind($this->container);
+    }
+
+    public function testCommandCacheIsUsedInProd(): void
+    {
+        // Basically just ensuring we cover the production case in this test
+        putenv('APP_ENV=production');
+        $config = self::getBaseConfig();
+        $config['aphiria']['console']['commandCachePath'] = '/commands';
+        GlobalConfiguration::addConfigurationSource(new HashTableConfiguration($config));
+        $this->binder->bind($this->container);
+    }
+
+    /**
+     * Gets the base config
+     *
+     * @return array The base config
+     */
+    private static function getBaseConfig(): array
+    {
+        return [
+            'aphiria' => [
+                'console' => [
+                    'annotationPaths' => ['/src'],
+                ]
+            ]
+        ];
+    }
+}
