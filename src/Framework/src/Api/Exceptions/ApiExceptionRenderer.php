ee2d3da1956292983a84ecb0844a5f861e754559
davidbyoung <dbyoung2@gmail.com>
2020-08-29 15:15:32 -0500

    WIP - Fixed the API exception renderer to use the exception response factory

--- src/Framework/src/Api/Exceptions/ApiExceptionRenderer.php
+++ src/Framework/src/Api/Exceptions/ApiExceptionRenderer.php
@@ -12,19 +12,11 @@ declare(strict_types=1);
 
 namespace Aphiria\Framework\Api\Exceptions;
 
-use Aphiria\Api\Errors\ProblemDetails;
-use Aphiria\Api\Errors\ProblemDetailsResponseMutator;
-use Aphiria\ContentNegotiation\MediaTypeFormatters\JsonMediaTypeFormatter;
-use Aphiria\ContentNegotiation\MediaTypeFormatters\SerializationException;
-use Aphiria\IO\Streams\Stream;
-use Aphiria\Net\Http\HttpException;
-use Aphiria\Net\Http\HttpStatusCodes;
 use Aphiria\Net\Http\IRequest;
 use Aphiria\Net\Http\IResponse;
 use Aphiria\Net\Http\IResponseFactory;
 use Aphiria\Net\Http\IResponseWriter;
 use Aphiria\Net\Http\Response;
-use Aphiria\Net\Http\StreamBody;
 use Aphiria\Net\Http\StreamResponseWriter;
 use Closure;
 use Exception;
@@ -71,15 +63,11 @@ class ApiExceptionRenderer implements IApiExceptionRenderer
      */
     public function createResponse(Exception $ex): IResponse
     {
-        try {
-            if ($this->request === null || $this->responseFactory === null) {
-                return $this->exceptionResponseFactory->createResponseWithoutContext($ex);
-            }
-
-            return $this->exceptionResponseFactory->createResponseWithContext($ex, $this->request, $this->responseFactory);
-        } catch (Exception $ex) {
-            return $this->createDefaultResponse($ex);
+        if ($this->request === null || $this->responseFactory === null) {
+            return $this->exceptionResponseFactory->createResponseWithoutContext($ex);
         }
+
+        return $this->exceptionResponseFactory->createResponseWithContext($ex, $this->request, $this->responseFactory);
     }
 
     /**
@@ -104,103 +92,5 @@ class ApiExceptionRenderer implements IApiExceptionRenderer
     public function setResponseFactory(IResponseFactory $responseFactory): void
     {
         $this->responseFactory = $responseFactory;
-    }
-
-    /**
-     * Creates the default error response
-     * Note: It is very important that this method never throws an exception
-     *
-     * @param Exception $ex The exception that was thrown
-     * @return IResponse The created response
-     */
-    protected function createDefaultResponse(Exception $ex): IResponse
-    {
-        return new Response(HttpStatusCodes::HTTP_INTERNAL_SERVER_ERROR);
-    }
-
-    /**
-     * Creates problem details from an exception
-     *
-     * @param Exception $ex The exception that was thrown
-     * @return ProblemDetails The problem details
-     */
-    protected function createProblemDetails(Exception $ex): ProblemDetails
-    {
-        return new ProblemDetails(
-            'https://tools.ietf.org/html/rfc7231#section-6.6.1',
-            'An error occurred',
-            null,
-            HttpStatusCodes::HTTP_INTERNAL_SERVER_ERROR
-        );
-    }
-
-    /**
-     * Creates a problem details response
-     *
-     * @param Exception $ex The exception that was thrown
-     * @return IResponse The created response
-     * @throws SerializationException Thrown if the problem details could not be serialized
-     * @throws HttpException Thrown if the response could not be created
-     */
-    protected function createProblemDetailsResponse(Exception $ex): IResponse
-    {
-        // Try to take advantage of the response factory
-        if ($this->responseFactory !== null && $this->request !== null) {
-            $response = $this->responseFactory->createResponse(
-                $this->request,
-                HttpStatusCodes::HTTP_INTERNAL_SERVER_ERROR,
-                null,
-                $this->createProblemDetails($ex)
-            );
-
-            return (new ProblemDetailsResponseMutator())->mutateResponse($response);
-        }
-
-        // We have to manually create a response
-        $response = new Response(HttpStatusCodes::HTTP_INTERNAL_SERVER_ERROR);
-        $response->getHeaders()->add('Content-Type', 'application/problem+json');
-        $bodyStream = new Stream(fopen('php://temp', 'r+b'));
-        // Intentionally using the parameterless constructor so that the default object encoder gets registered
-        $mediaTypeFormatter = new JsonMediaTypeFormatter();
-        $mediaTypeFormatter->writeToStream($this->createProblemDetails($ex), $bodyStream, null);
-        $response->setBody(new StreamBody($bodyStream));
-
-        return $response;
-    }
-
-    /**
-     * Creates a response without a request context
-     *
-     * @param Exception $ex The exception that was thrown
-     * @return IResponse The creates request
-     * @throws HttpException Thrown if the response could not be created
-     * @throws SerializationException Thrown if the problem details could not be serialized
-     */
-    protected function createResponseWithoutRequest(Exception $ex): IResponse
-    {
-        if ($this->useProblemDetails) {
-            return $this->createProblemDetailsResponse($ex);
-        }
-
-        return $this->createDefaultResponse($ex);
-    }
-
-    /**
-     * Creates a response with a request context
-     *
-     * @param Exception $ex The exception that was thrown
-     * @param IRequest $request The current request
-     * @return IResponse The creates request
-     * @throws HttpException Thrown if the response could not be created
-     * @throws SerializationException Thrown if the problem details could not be serialized
-     */
-    protected function createResponseWithRequest(Exception $ex, IRequest $request): IResponse
-    {
-        if ($this->responseFactory !== null && isset($this->responseFactories[\get_class($ex)])) {
-            return $this->responseFactories[\get_class($ex)]($ex, $request, $this->responseFactory);
-        }
-
-        // We can't do much even with the request if we cannot negotiate it
-        return $this->createResponseWithoutRequest($ex);
     }
 }
