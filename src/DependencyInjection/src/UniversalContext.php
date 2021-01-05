a4720968eaf16e6369446059b8eaaefc797b4990
David Young <dbyoung2@gmail.com>
2021-01-04 20:08:08 -0600

    Updated copyright years (#103)

--- src/DependencyInjection/src/UniversalContext.php
+++ src/DependencyInjection/src/UniversalContext.php
@@ -4,7 +4,7 @@
  * Aphiria
  *
  * @link      https://www.aphiria.com
- * @copyright Copyright (C) 2020 David Young
+ * @copyright Copyright (C) 2021 David Young
  * @license   https://github.com/aphiria/aphiria/blob/1.x/LICENSE.md
  */
 
