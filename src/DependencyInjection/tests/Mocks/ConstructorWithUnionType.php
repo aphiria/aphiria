7da72b953ac1646ba02bcb870073ae96acee2d11
David Young <dbyoung2@gmail.com>
2021-01-18 19:27:45 -0600

    Fixed file headers

--- src/DependencyInjection/tests/Mocks/ConstructorWithUnionType.php
+++ src/DependencyInjection/tests/Mocks/ConstructorWithUnionType.php
@@ -5,7 +5,7 @@
  *
  * @link      https://www.aphiria.com
  * @copyright Copyright (C) 2021 David Young
- * @license   https://github.com/aphiria/aphiria/blob/master/LICENSE.md
+ * @license   https://github.com/aphiria/aphiria/blob/1.x/LICENSE.md
  */
 
 declare(strict_types=1);
