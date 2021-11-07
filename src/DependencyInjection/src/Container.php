2218f5fcdecd660379f3957b9f89d8d5eb425407
David Young <dbyoung2@gmail.com>
2021-11-06 19:57:41 -0500

    Fixed merge conflicts

--- src/DependencyInjection/src/Container.php
+++ src/DependencyInjection/src/Container.php
@@ -186,7 +186,7 @@ class Container implements IContainer
     {
         if (
             $this->currentContext->isTargeted
-            && $this->hasTargetedBinding($interface, $this->currentContext->getTargetClass())
+            && $this->hasTargetedBinding($interface, $this->currentContext->targetClass)
         ) {
             return true;
         }
@@ -339,7 +339,7 @@ class Container implements IContainer
                         \sprintf(
                             '%s is not instantiable%s',
                             $className,
-                            $this->currentContext->isTargeted ? " (dependency of {$this->currentContext->getTargetClass()})" : ''
+                            $this->currentContext->isTargeted ? " (dependency of {$this->currentContext->targetClass})" : ''
                         )
                     );
                 }
