007fc0fc7bae0faff92d7e05592f7ecab498bc7a
David Young <dbyoung2@gmail.com>
2023-04-05 19:55:19 -0500

    Fixed Psalm error

--- src/ContentNegotiation/src/MediaTypeFormatterMatcher.php
+++ src/ContentNegotiation/src/MediaTypeFormatterMatcher.php
@@ -89,6 +89,7 @@ final class MediaTypeFormatterMatcher implements IMediaTypeFormatterMatcher
     ): ?MediaTypeFormatterMatch {
         // Rank the media type headers if they are rankable
         if (\count($mediaTypeHeaders) > 0 && $mediaTypeHeaders[0] instanceof AcceptMediaTypeHeaderValue) {
+            /** @var list<AcceptMediaTypeHeaderValue> $mediaTypeHeaders */
             $mediaTypeHeaders = $this->rankAcceptMediaTypeHeaders($mediaTypeHeaders);
         }
 
