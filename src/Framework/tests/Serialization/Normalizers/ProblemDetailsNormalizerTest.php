7acd7e3f759c112e3dfbfdae915110c920417fbb
davidbyoung <dbyoung2@gmail.com>
2023-05-14 03:49:43 -0500

    Fixed some usages of now-final classes in the Symfony serializer component

--- src/Framework/tests/Serialization/Normalizers/ProblemDetailsNormalizerTest.php
+++ src/Framework/tests/Serialization/Normalizers/ProblemDetailsNormalizerTest.php
@@ -17,16 +17,65 @@ use Aphiria\Framework\Serialization\Normalizers\ProblemDetailsNormalizer;
 use Aphiria\Net\Http\HttpStatusCode;
 use PHPUnit\Framework\TestCase;
 use Symfony\Component\Serializer\Exception\InvalidArgumentException;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
 use Symfony\Component\Serializer\Serializer;
 
 class ProblemDetailsNormalizerTest extends TestCase
 {
     private ProblemDetailsNormalizer $normalizer;
+    private ObjectNormalizer $objectNormalizer;
 
     protected function setUp(): void
     {
-        $this->normalizer = new ProblemDetailsNormalizer();
+        $this->objectNormalizer = new ObjectNormalizer();
+        $this->normalizer = new ProblemDetailsNormalizer($this->objectNormalizer);
         $this->normalizer->setSerializer(new Serializer([$this->normalizer], []));
+    }
+
+    public function testCallingDenormalizePassesCallThroughToObjectNormalizer(): void
+    {
+        $normalizedData = [
+            'type' => 'foo',
+            'title' => 'title',
+            'detail' => 'details',
+            'status' => HttpStatusCode::InternalServerError->value,
+            'instance' => 'instance',
+            'extensions' => ['foo' => 'bar']
+        ];
+        $expectedProblemDetails = new ProblemDetails('foo', 'title', 'details', HttpStatusCode::InternalServerError, 'instance', ['foo' => 'bar']);
+        $this->assertEquals(
+            $expectedProblemDetails,
+            $this->objectNormalizer->denormalize($normalizedData, ProblemDetails::class)
+        );
+        $this->assertEquals(
+            $expectedProblemDetails,
+            $this->normalizer->denormalize($normalizedData, ProblemDetails::class)
+        );
+    }
+
+    public function testCallingDynamicMethodPassesCallThroughToObjectNormalizer(): void
+    {
+        // TODO
+    }
+
+    public function testCallingGetSupportedTypesPassesCallThroughToObjectNormalizer(): void
+    {
+        // TODO
+    }
+
+    public function testCallingGetSupportsNormalizationPassesCallThroughToObjectNormalizer(): void
+    {
+        // TODO
+    }
+
+    public function testCallingSetSerializerPassesCallThroughToObjectNormalizer(): void
+    {
+        // TODO
+    }
+
+    public function testCallingSupportsDenormalizationPassesCallThroughToObjectNormalizer(): void
+    {
+        // TODO
     }
 
     public function testNormalizeAddsExtensionsToTopLevelOfNormalizedArray(): void
