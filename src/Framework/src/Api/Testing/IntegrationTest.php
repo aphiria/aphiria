123577946a812f4ac49643a381f4c56b3099d3d2
davidbyoung <dbyoung2@gmail.com>
2023-08-09 17:12:58 -0500

    Fixed nullability of authenticator

--- src/Framework/src/Api/Testing/IntegrationTest.php
+++ src/Framework/src/Api/Testing/IntegrationTest.php
@@ -123,12 +123,14 @@ trait IntegrationTest
      * Creates the authenticator
      *
      * @param IContainer $container The DI container
-     * @return IAuthenticator The authenticator
-     * @throws ResolutionException Thrown if the authenticator could not be resolved
+     * @return IAuthenticator|null The authenticator if one could be created, otherwise null
      */
-    protected function createAuthenticator(IContainer $container): IAuthenticator
+    protected function createAuthenticator(IContainer $container): IAuthenticator|null
     {
-        return $container->resolve(IAuthenticator::class);
+        $authenticator = null;
+        $container->tryResolve(IAuthenticator::class, $authenticator);
+
+        return $authenticator;
     }
 
     /**
