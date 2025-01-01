1ff3fcf32784e6a8abfaa2563a4c5c9bb7dfbd24
davidbyoung <dbyoung2@gmail.com>
2025-01-01 15:42:04 -0600

    Happy New Year

--- src/Api/src/Controllers/ControllerParameterResolver.php
+++ src/Api/src/Controllers/ControllerParameterResolver.php
@@ -4,7 +4,7 @@
  * Aphiria
  *
  * @link      https://www.aphiria.com
- * @copyright Copyright (C) 2024 David Young
+ * @copyright Copyright (C) 2025 David Young
  * @license   https://github.com/aphiria/aphiria/blob/1.x/LICENSE.md
  */
 
@@ -21,7 +21,6 @@ use Aphiria\Net\Http\IRequest;
 use Aphiria\Routing\Attributes\Header;
 use Aphiria\Routing\Attributes\QueryString;
 use Aphiria\Routing\Attributes\RouteVariable;
-use ArrayAccess;
 use Closure;
 use ReflectionNamedType;
 use ReflectionParameter;
