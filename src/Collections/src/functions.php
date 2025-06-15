7d867b7506e9015cc70ac101eda8a8bafb45a6f1
David Young <dbyoung2@gmail.com>
2025-06-15 16:57:30 -0500

    Added more collection functions, tweaked CS and tests (#343)
    * Added more collection functions, tweaked CS and tests
    
    * Updated CHANGELOG

--- src/Collections/src/functions.php
+++ src/Collections/src/functions.php
@@ -13,15 +13,99 @@ declare(strict_types=1);
 namespace Aphiria\Collections\Functions;
 
 use Aphiria\Collections\ArrayList;
+use Aphiria\Collections\HashSet;
+use Aphiria\Collections\HashTable;
+use Aphiria\Collections\ImmutableArrayList;
+use Aphiria\Collections\ImmutableHashSet;
+use Aphiria\Collections\ImmutableHashTable;
+use Aphiria\Collections\KeyValuePair;
+use Aphiria\Collections\Queue;
+use Aphiria\Collections\Stack;
 
 /**
  * Creates an ArrayList from an array of values
  *
  * @template T
- * @param  array<T>  $values  The values to add to the list
+ * @param list<T> $values The values to add to the list
  * @return ArrayList<T> The created list
  */
-function array_list(array $values = []): ArrayList
-{
+function array_list(array $values = []): ArrayList {
     return new ArrayList($values);
+}
+
+/**
+ * Creates a HashSet from an array of values
+ *
+ * @template T
+ * @param list<T> $values The values to add to the hash set
+ * @return HashSet<T> The created hash set
+ */
+function hash_set(array $values = []): HashSet {
+    return new HashSet($values);
+}
+
+/**
+ * Creates a HashTable from an array of key-value pairs
+ *
+ * @template TKey
+ * @template TValue
+ * @param list<KeyValuePair<TKey, TValue>> $kvps The values to add to the hash table
+ * @return HashTable<TKey, TValue> The created hash table
+ */
+function hash_table(array $kvps = []): HashTable {
+    return new HashTable($kvps);
+}
+
+/**
+ * Creates an ImmutableArrayList from an array of values
+ *
+ * @template T
+ * @param list<T> $values The values to add to the list
+ * @return ImmutableArrayList<T> The created list
+ */
+function immutable_array_list(array $values = []): ImmutableArrayList {
+    return new ImmutableArrayList($values);
+}
+
+/**
+ * Creates an ImmutableHashSet from an array of values
+ *
+ * @template T
+ * @param list<T> $values The values to add to the hash set
+ * @return ImmutableHashSet<T> The created hash set
+ */
+function immutable_hash_set(array $values = []): ImmutableHashSet {
+    return new ImmutableHashSet($values);
+}
+
+/**
+ * Creates an ImmutableHashTable from an array of key-value pairs
+ *
+ * @template TKey
+ * @template TValue
+ * @param list<KeyValuePair<TKey, TValue>> $kvps The values to add to the hash table
+ * @return ImmutableHashTable<TKey, TValue> The created hash table
+ */
+function immutable_hash_table(array $kvps = []): ImmutableHashTable {
+    return new ImmutableHashTable($kvps);
+}
+
+/**
+ * Creates a Queue
+ *
+ * @template T
+ * @return Queue<T> The created queue
+ */
+function queue(): Queue {
+    return new Queue();
+}
+
+/**
+ * Creates a Stack
+ *
+ * @template T
+ * @return Stack<T> The created stack
+ */
+function stack(): Stack {
+    return new Stack();
 }
--- src/Collections/tests/FunctionsTest.php
+++ src/Collections/tests/FunctionsTest.php
@@ -1,10 +1,27 @@
 <?php
 
+/**
+ * Aphiria
+ *
+ * @link      https://www.aphiria.com
+ * @copyright Copyright (C) 2025 David Young
+ * @license   https://github.com/aphiria/aphiria/blob/1.x/LICENSE.md
+ */
+
+declare(strict_types=1);
+
 namespace Aphiria\Collections\Tests;
 
-use Aphiria\Collections\ArrayList;
+use Aphiria\Collections\KeyValuePair;
 use PHPUnit\Framework\TestCase;
 use function Aphiria\Collections\Functions\array_list;
+use function Aphiria\Collections\Functions\hash_set;
+use function Aphiria\Collections\Functions\hash_table;
+use function Aphiria\Collections\Functions\immutable_array_list;
+use function Aphiria\Collections\Functions\immutable_hash_set;
+use function Aphiria\Collections\Functions\immutable_hash_table;
+use function Aphiria\Collections\Functions\queue;
+use function Aphiria\Collections\Functions\stack;
 
 class FunctionsTest extends TestCase
 {
@@ -12,42 +29,129 @@ class FunctionsTest extends TestCase
     {
         $values = ['foo', 'bar'];
         $list = array_list($values);
-        $this->assertInstanceOf(ArrayList::class, $list);
         $this->assertEquals($values, $list->toArray());
     }
 
     public function testArrayListFunctionCreatesEmptyArrayListWhenNoValuesProvided(): void
     {
-        $list = array_list();
-        $this->assertInstanceOf(ArrayList::class, $list);
-        $this->assertCount(0, $list);
-    }
-
-    public function testArrayListFunctionMaintainsTypeSafety(): void
-    {
-        $values = [1, 2, 3];
-        $list = array_list($values);
-        $this->assertSame($values, $list->toArray());
-        $this->assertSame(1, $list->get(0));
-    }
-
-    public function testArrayListFunctionWorksWithMixedTypes(): void
-    {
-        $values = [1, 'foo', true];
-        $list = array_list($values);
-        $this->assertEquals($values, $list->toArray());
+        $this->assertCount(0, array_list());
     }
 
     public function testArrayListFunctionReturnsNewInstanceEachCall(): void
     {
-        $list1 = array_list(['foo']);
-        $list2 = array_list(['bar']);
-        $this->assertEquals(['foo'], $list1->toArray());
-        $this->assertEquals(['bar'], $list2->toArray());
+        $this->assertNotSame(array_list(['foo']), array_list(['foo']));
+    }
 
-        // Creating new collections so we can have 100% that are not the same
-        $newInstance1 = array_list(['foo']);
-        $newInstance2 = array_list(['foo']);
-        $this->assertNotSame($newInstance1, $newInstance2);
+    public function testHashSetFunctionCreatesHashSetWithValues(): void
+    {
+        $values = ['foo', 'bar'];
+        $hashSet = hash_set($values);
+        $this->assertEquals($values, $hashSet->toArray());
+    }
+
+    public function testHashSetFunctionCreatesEmptyHashSetWhenNoValuesProvided(): void
+    {
+        $this->assertCount(0, hash_set());
+    }
+
+    public function testHashSetFunctionReturnsNewInstanceEachCall(): void
+    {
+        $this->assertNotSame(hash_set(['foo']), hash_set(['foo']));
+    }
+
+    public function testHashTableFunctionCreatesHashTableWithValues(): void
+    {
+        $kvps = [new KeyValuePair('foo', 'bar'),  new KeyValuePair('baz', 'blah')];
+        $hashTable = hash_table($kvps);
+        $this->assertEquals($kvps, $hashTable->toArray());
+    }
+
+    public function testHashTableFunctionCreatesEmptyHashTableWhenNoValuesProvided(): void
+    {
+        $this->assertCount(0, hash_table());
+    }
+
+    public function testHashTableFunctionReturnsNewInstanceEachCall(): void
+    {
+        $kvps = [new KeyValuePair('foo', 'bar')];
+        $this->assertNotSame(hash_table($kvps), hash_table($kvps));
+    }
+
+    public function testImmutableArrayListFunctionCreatesImmutableArrayListWithValues(): void
+    {
+        $values = ['foo', 'bar'];
+        $list = immutable_array_list($values);
+        $this->assertEquals($values, $list->toArray());
+    }
+
+    public function testImmutableArrayListFunctionCreatesEmptyImmutableArrayListWhenNoValuesProvided(): void
+    {
+        $this->assertCount(0, immutable_array_list());
+    }
+
+    public function testImmutableArrayListFunctionReturnsNewInstanceEachCall(): void
+    {
+        $this->assertNotSame(immutable_array_list(['foo']), immutable_array_list(['foo']));
+    }
+
+    public function testImmutableHashSetFunctionCreatesImmutableHashSetWithValues(): void
+    {
+        $values = ['foo', 'bar'];
+        $hashSet = immutable_hash_set($values);
+        $this->assertEquals($values, $hashSet->toArray());
+    }
+
+    public function testImmutableHashSetFunctionCreatesEmptyImmutableHashSetWhenNoValuesProvided(): void
+    {
+        $this->assertCount(0, immutable_hash_set());
+    }
+
+    public function testImmutableHashSetFunctionReturnsNewInstanceEachCall(): void
+    {
+        $this->assertNotSame(immutable_hash_set(['foo']), immutable_hash_set(['foo']));
+    }
+
+    public function testImmutableHashTableFunctionCreatesHashTableWithValues(): void
+    {
+        $kvps = [new KeyValuePair('foo', 'bar'),  new KeyValuePair('baz', 'blah')];
+        $hashTable = immutable_hash_table($kvps);
+        $this->assertEquals($kvps, $hashTable->toArray());
+    }
+
+    public function testImmutableHashTableFunctionCreatesEmptyHashTableWhenNoValuesProvided(): void
+    {
+        $this->assertCount(0, immutable_hash_table());
+    }
+
+    public function testImmutableHashTableFunctionReturnsNewInstanceEachCall(): void
+    {
+        $kvps = [new KeyValuePair('foo', 'bar')];
+        $this->assertNotSame(hash_table($kvps), hash_table($kvps));
+    }
+
+    public function testQueueFunctionCreatesQueueWithValues(): void
+    {
+        $queue = queue();
+        $queue->enqueue('foo');
+        $queue->enqueue('bar');
+        $this->assertEquals(['foo', 'bar'], $queue->toArray());
+    }
+
+    public function testQueueFunctionReturnsNewInstanceEachCall(): void
+    {
+        $this->assertNotSame(queue(), queue());
+    }
+
+    public function testStackFunctionCreatesStackWithValues(): void
+    {
+        $stack = stack();
+        $stack->push('foo');
+        $stack->push('bar');
+        $this->assertEquals(['bar', 'foo'], $stack->toArray());
+    }
+
+    public function testStackFunctionReturnsNewInstanceEachCall(): void
+    {
+        $this->assertNotSame(stack(), stack());
     }
 }
