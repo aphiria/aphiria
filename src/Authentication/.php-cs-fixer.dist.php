2908871f4fcddd3118fb8fe39c145cd7f7d923d8
davidbyoung <dbyoung2@gmail.com>
2025-07-13 19:35:28 -0500

    Tweaked PHP-CS-Fixer rule, fixed typo

--- src/Authentication/.php-cs-fixer.dist.php
+++ src/Authentication/.php-cs-fixer.dist.php
@@ -26,7 +26,7 @@ return new PhpCsFixer\Config()
         'method_argument_space' => ['on_multiline' => 'ensure_fully_multiline'],
         // A very tiny micro-optimization to reduce the number of opcodes for native function calls
         'native_function_invocation' => ['include' => ['@all']],
-        'new_with_parentheses ' => true,
+        'new_with_parentheses' => true,
         'no_empty_comment' => true,
         'no_empty_statement' => true,
         'no_leading_import_slash' => true,
