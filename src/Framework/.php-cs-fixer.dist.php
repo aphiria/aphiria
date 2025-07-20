d644c5ef54379c4ff3162d9642a2a728cfd19d4d
davidbyoung <dbyoung2@gmail.com>
2025-07-20 15:13:26 -0500

    Missed one PHP-CS-Fixer config setting that was not compliant with coding style

--- src/Framework/.php-cs-fixer.dist.php
+++ src/Framework/.php-cs-fixer.dist.php
@@ -74,7 +74,7 @@ return new PhpCsFixer\Config()
                 'method_private_static',
                 'method_private',
             ],
-            'sort_algorithm' => 'alpha'
+            'sort_algorithm' => 'alpha',
         ],
         'ordered_imports' => true,
         'return_type_declaration' => ['space_before' => 'none'],
