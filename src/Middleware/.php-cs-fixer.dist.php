2758a3e8bd9fc333ededa328282de85acbbb5804
davidbyoung <dbyoung2@gmail.com>
2025-11-29 16:17:24 -0600

    Updated individual libraries' CS ruleset name to latest version

--- src/Middleware/.php-cs-fixer.dist.php
+++ src/Middleware/.php-cs-fixer.dist.php
@@ -11,7 +11,7 @@ EOT;
 return new PhpCsFixer\Config()
     ->setUnsupportedPhpVersionAllowed(true)
     ->setRules([
-        '@PER-CS2.0' => true,
+        '@PER-CS2x0' => true,
         'array_syntax' => ['syntax' => 'short'],
         'blank_line_after_opening_tag' => true,
         'compact_nullable_type_declaration' => true,
